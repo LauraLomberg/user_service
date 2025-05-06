be47da7dc66e51b1f5630e027181faf5693a624b
Graccio <sa.ananyev@yandex.ru>
2025-05-06 14:12:27 +0300

    feature/BJS2-66383-created-profilepicevent-publisher (#2944)
    * BJS2-66383 created ProfilePicEventPublisher
    
    * BJS2-66383 made a merger of two RedisConfig
    
    * BJS2-66383 minor fix
    
    * BJS2-66383 minor fix

--- src/main/java/school/faang/user_service/config/RedisConfig.java
+++ src/main/java/school/faang/user_service/config/RedisConfig.java
@@ -5,10 +5,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import school.faang.user_service.service.UserService;
 
 @Configuration
 @RequiredArgsConstructor
@@ -21,9 +26,32 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int redisPort;
 
+    @Value("${spring.data.redis.channel.user-ban}")
+    private String userBanChannelName;
+
     @Bean
     public JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory(new RedisStandaloneConfiguration(redisHost, redisPort));
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisHost, redisPort);
+        return new JedisConnectionFactory(config);
+    }
+
+    @Bean
+    public RedisMessageListenerContainer listenerContainer(
+            RedisConnectionFactory factory,
+            MessageListenerAdapter adapter
+    ) {
+        log.info("Creating RedisMessageListenerContainer for topic '{}'", userBanChannelName);
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(factory);
+        container.addMessageListener(adapter, new ChannelTopic(userBanChannelName));
+        return container;
+    }
+
+    @Bean
+    public MessageListenerAdapter messageListener(UserService userService) {
+        MessageListenerAdapter adapter = new MessageListenerAdapter(userService, "banUser");
+        adapter.setSerializer(new StringRedisSerializer());
+        return adapter;
     }
 
     @Bean
